e73733a0688330f30c28edd7635307eb41e0b12c
shadowchowwc <121296152+shadowchowwc@users.noreply.github.com>
2023-12-08 22:51:36 +0800

    Update che-piao-piao.js

--- premium/che-piao-piao.js
+++ premium/che-piao-piao.js
@@ -4,8 +4,8 @@
 
 [rewrite_local]
 
-# > RevenueCat
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/shadowchowwc/quantumult-x/main/premium/che-piao-piao.js
+# ～ RevenueCat@ddgksf2013
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
 ^https:\/\/api\.revenuecat\.com\/.+\/subscribers\/[^/]+/(offerings|attributes)$ url request-header (\r\n)X-RevenueCat-ETag:.+(\r\n) request-header $1X-RevenueCat-ETag:$2
 
 [mitm]
@@ -15,13 +15,13 @@ hostname=api.revenuecat.com
 ***********************************/
 
 var ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
-var cuttlefish = {"Attention":"Congratulations!","request_date_ms":1662599120248,"request_date":"2022-09-08T01:05:20Z","subscriber":{"non_subscriptions":{},"first_seen":"2022-09-08T01:04:03Z","original_application_version":"8","other_purchases":{},"management_url":"https://apps.apple.com/account/subscriptions","subscriptions":{},"entitlements":{},"original_purchase_date":"2022-09-07T13:05:43Z","original_app_user_id":"$RCAnonymousID:shadow","last_seen":"2022-09-08T01:04:03Z"}};
-var shadow={"is_sandbox":false,"ownership_type":"PURCHASED","billing_issues_detected_at":null,"period_type":"normal","expires_date":"2099-12-18T01:04:17Z","grace_period_expires_date":null,"unsubscribe_detected_at":null,"original_purchase_date":"2022-09-08T01:04:18Z","purchase_date":"2022-09-08T01:04:17Z","store":"app_store"};
-var shadow={"grace_period_expires_date":null,"purchase_date":"2022-09-08T01:04:17Z","product_identifier":"shadow_1y_128","expires_date":"2099-12-18T01:04:17Z"};
-var obj = JSON.parse(JSON.stringify(cuttlefish));shadow['product_identifier']="com.shadow.premium.yearly";obj['subscriber']['subscriptions']['com.shadow.premium.yearly']=shadow;
+var cuttlefish = {"Attention":"Congratulations!","request_date_ms":1662599120248,"request_date":"2022-09-08T01:05:20Z","subscriber":{"non_subscriptions":{},"first_seen":"2022-09-08T01:04:03Z","original_application_version":"8","other_purchases":{},"management_url":"https://apps.apple.com/account/subscriptions","subscriptions":{},"entitlements":{},"original_purchase_date":"2022-09-07T13:05:43Z","original_app_user_id":"$RCAnonymousID:ddgksf2013","last_seen":"2022-09-08T01:04:03Z"}};
+var ddgksf2013={"is_sandbox":false,"ownership_type":"PURCHASED","billing_issues_detected_at":null,"period_type":"normal","expires_date":"2099-12-18T01:04:17Z","grace_period_expires_date":null,"unsubscribe_detected_at":null,"original_purchase_date":"2022-09-08T01:04:18Z","purchase_date":"2022-09-08T01:04:17Z","store":"app_store"};
+var ddgksf2021={"grace_period_expires_date":null,"purchase_date":"2022-09-08T01:04:17Z","product_identifier":"ddgksf2013_1y_128","expires_date":"2099-12-18T01:04:17Z"};
+var obj = JSON.parse(JSON.stringify(cuttlefish));ddgksf2021['product_identifier']="com.ddgksf2013.premium.yearly";obj['subscriber']['subscriptions']['com.ddgksf2013.premium.yearly']=ddgksf2013;
 
-if(ua.indexOf('%E8%BD%A6%E7%A5%A8%E7%A5%A8') != -1) {obj['subscriber']['entitlements']['vip+watch_vip']=shadow;}
+if(ua.indexOf('%E8%BD%A6%E7%A5%A8%E7%A5%A8') != -1) {obj['subscriber']['entitlements']['vip+watch_vip']=ddgksf2021;}
 else{
-  obj['subscriber']['entitlements']['pro']=shadow;
+  obj['subscriber']['entitlements']['pro']=ddgksf2021;
 }
 $done({body: JSON.stringify(obj)});
